4d208781ca9b1fe1582e3917cb0c8476b20ed64d
TiyTiy474 <baiyrachnyimaksim@gmail.com>
2024-12-14 10:20:06 +0300

    Файловый менеджер

---
+++ WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+    //TODO: сделать файловый менеджер
+    //TODO: сделать кнопку на примере диске 
+    //TODO: с помощью двойного щелчка открывается эта залупень
+    //TODO: была работа с содержимым в данном проводнике 
+    //TODO: 
+namespace WpfApp7;
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
